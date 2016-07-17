366067caf6ce9d674bad11f59847baebd34f0b1f
pythonstory <pincoin.info@gmail.com>
2016-07-18 03:47:50 +0900

    dbcp2, spring-data-jpa 설정

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # rabiang
 
 blog, forum, and e-commerce based on Spring Web MVC
+
+## Requirements
+
+* jstl / jsp
+* spring-webmvc
+* spring-data-jpa
+* logback / slf4j
---
+++ src/main/java/net/rabiang/model/Post.java
@@ -0,0 +1,70 @@
+package net.rabiang.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String title;
+
+	private String slug;
+
+	private String body;
+
+	private int status;
+
+	private int format;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSlug() {
+		return slug;
+	}
+
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getFormat() {
+		return format;
+	}
+
+	public void setFormat(int format) {
+		this.format = format;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + "]";
+	}
+
+}
