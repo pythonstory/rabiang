3f8c56c31a9949d9f7d64d4e9ca8cee79c7b1006
pythonstory <pincoin.info@gmail.com>
2016-07-18 06:48:02 +0900

    Post service, repository 처리

--- src/main/java/net/rabiang/repository/PostRepository.java
+++ src/main/java/net/rabiang/repository/PostRepository.java
@@ -1,15 +1,16 @@
 package net.rabiang.repository;
 
-import java.util.List;
+import java.util.Collection;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.Repository;
 
 import net.rabiang.model.Post;
 
 public interface PostRepository extends Repository<Post, Long> {
 
-	public List<Post> findAll();
+	public Collection<Post> findAll() throws DataAccessException;
 	
-	public Post save(Post post);
+	public Post save(Post post) throws DataAccessException;
 
 }
--- src/main/java/net/rabiang/web/PostController.java
+++ src/main/java/net/rabiang/web/PostController.java
@@ -1,17 +1,29 @@
 package net.rabiang.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import net.rabiang.service.PostService;
+
 @Controller
 @RequestMapping("/post")
 public class PostController {
+	
+	private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String postIndex(Model model) {
+		this.postService.findPosts();
+		
 		return "post/index";
 	}
 
