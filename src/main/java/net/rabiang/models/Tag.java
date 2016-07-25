debb672b91754c0563aea2825d1c3c49e429560b
pythonstory <pincoin.info@gmail.com>
2016-07-26 04:43:14 +0900

    post-tag bidirectional

--- src/main/java/net/rabiang/models/Tag.java
+++ src/main/java/net/rabiang/models/Tag.java
@@ -1,5 +1,6 @@
 package net.rabiang.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,7 +11,15 @@ import javax.persistence.ManyToMany;
 public class Tag extends NamedEntity {
 
 	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "tags")
-	private List<Post> posts;
+	private List<Post> posts = new ArrayList<Post>();
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public String toString() {
